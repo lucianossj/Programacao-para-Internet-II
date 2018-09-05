00d1f690528b10a14efbcdf696067c169c4b361a
LUCIANO DA SILVA SANTOS JUNIOR <631610257@fspoaeduc.com.br>
2018-09-05 11:06:51 -0300

    Aula 05 - Introdução ao BD (Correção).

--- app-produtos-GET/app-produtos/produtos.php
+++ app-produtos-GET/app-produtos/produtos.php
@@ -73,11 +73,25 @@
         }
 	}
 
-	function inserir(Produto $produto)
-    {
-        $GLOBALS['produtos'][] = $produto;
-    }
-	
+	function inserir (Produto $produto){
+		
+		$conexao = "mysql:host=localhost; dbname=app-produtos";
+		$usuario = "root";
+		$senha = "";
+		
+		$pdo = new PDO($conexao, $usuario, $senha);
 
- 
+		$q = "INSERT INTO produtos (nome, preco) VALUES (:nome, :preco)";
+		
+		$comando = $pdo->prepare($q);
+
+		$comando->bindParam(":nome", $produto->nome);
+		$comando->bindParam(":preco", $produto->preco);
+		
+		$comando->execute();
+		
+		var_dump($pdo->lastInsertId());
+		
+	}
+		
 	?>
