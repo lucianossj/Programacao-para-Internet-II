25b8884d6924c29bc1fad1bd962cb6ab5035e457
Bryan <bryan.eduardo9@gmail.com>
2018-10-25 02:03:32 -0300

    Aula 12 - AJAX

--- bmmotores/bmmotores.js
+++ bmmotores/bmmotores.js
@@ -1,10 +1,23 @@
-
 var body = document.querySelector("#produtos");
+body.onload = iniciaAplicacao;
 
-body.onload = iniciaAplicacao();
+var contador = 0;
 
 function iniciaAplicacao(){
-
-    console.log("Iniciando aplicação...");
-
+    console.log("Iniciando Aplicação ....");
+    setInterval(loadJSON,5000);
 }
+
+function loadJSON(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(contador+"->"+this.responseText);
+            contador++;
+       }
+    };
+    xhttp.open("GET", "bmmotores.json", true);
+    xhttp.send();
+        
+}
+
