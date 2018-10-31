9c5f1dd7913a338f8d7f1f4c052b55b0cf156e7d
LUCIANO DA SILVA SANTOS JUNIOR <631610257@fspoaeduc.com.br>
2018-10-31 10:12:34 -0300

    Puxando nome dos motores e imagem do arquivo JSON com AJAX.

--- bmmotores/bmmotores.js
+++ bmmotores/bmmotores.js
@@ -2,6 +2,8 @@ var body = document.querySelector("#produtos");
 body.onload = iniciaAplicacao;
 
 var contador = 0;
+var objetoMotores;
+var str;
 
 function iniciaAplicacao(){
     console.log("Iniciando Aplicação ....");
@@ -12,10 +14,18 @@ function loadJSON(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(contador+"->"+this.responseText);
+            //console.log(contador+"->"+this.responseText);
+
+            objetoMotores = JSON.parse(this.responseText);
+            
+            str += "<dl> <dd> <figure> <img src="+objetoMotores[contador].imagem+" /> </figure> </dd> <dt> <a href='#'> "+ objetoMotores[contador].nome + "</a> </dt> </dl>";
+            
+            document.querySelector("main").innerHTML = str;
+
             contador++;
        }
     };
+
     xhttp.open("GET", "bmmotores.json", true);
     xhttp.send();
         
